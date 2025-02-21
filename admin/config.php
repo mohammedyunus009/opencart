aaf6cb95ace1755554fa9efedafc2538a1df90ad
hitech <mohammedyunus009@gmail.com>
2025-02-21 12:59:37 +0000

    direct files f

---
+++ admin/config.php
@@ -0,0 +1,35 @@
+<?php
+// APPLICATION
+define('APPLICATION', 'Admin');
+
+// HTTP
+define('HTTP_SERVER', 'http://localhost/admin/');
+define('HTTP_CATALOG', 'http://localhost/');
+// DIR
+define('DIR_OPENCART', '/var/www/html/');
+define('DIR_APPLICATION', DIR_OPENCART . 'admin/');
+define('DIR_SYSTEM', DIR_OPENCART . 'system/');
+define('DIR_EXTENSION', DIR_OPENCART . 'extension/');
+define('DIR_IMAGE', DIR_OPENCART . 'image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_CATALOG', DIR_OPENCART . 'catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'mysql');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'opencart');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
+define('DB_PORT', '3306');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
